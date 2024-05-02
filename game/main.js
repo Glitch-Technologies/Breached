b8cf585bd9242233a8e8db3130d09fcd8b524a00
greenturtle537 <greenturtle537@mail.com>
2024-05-02 13:05:48 -0400

    Fix async issue in draw function

--- game/main.js
+++ game/main.js
@@ -7,6 +7,18 @@ document.body.appendChild(canvas);
 
 let ctx = canvas.getContext('2d'), x = canvas.width / 2, y = canvas.height / 2, rotation = 0;
 
+// Bad implementation of image drawing to canvas
+// Must be async
+async function draw() {
+    const img = new Image();
+    img.src = "file.png";
+
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0, w, h);
+        renderMap(0,w,h,ctx,0,0);            
+    }
+}
+
 // Function to draw the cube
 function drawCube() {
     // Clear the canvas
